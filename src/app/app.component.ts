8cdf2607773f9978ef1ecee734604820b6f5e0fa
arun.yorker <arun.yorker@gmail.com>
2020-11-24 16:14:40 +0530

    camera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from '@capacitor/core';
+const { SplashScreen } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -10,18 +8,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    /* To make sure we provide the fastest app loading experience 
+       for our users, hide the splash screen automatically 
+       when the app is ready to be used:
+        
+        https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
+    */
+    SplashScreen.hide();
   }
 }
