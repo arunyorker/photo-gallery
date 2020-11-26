8c91036f244d79f97fc1c723264d6ceaaed6450d
arun.yorker <arun.yorker@gmail.com>
2020-11-26 13:34:30 +0530

    fingerprint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FingerprintAIO} from '@ionic-native/fingerprint-aio/ngx'
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },FingerprintAIO],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
 import { Photo, PhotoService } from '../services/photo.service';
+import {FingerprintAIO} from '@ionic-native/fingerprint-aio/ngx'
 
 @Component({
   selector: 'app-tab2',
@@ -9,7 +10,8 @@ import { Photo, PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController,
+    private faio:FingerprintAIO) {}
 
   async ngOnInit() {
     await this.photoService.loadSaved();
@@ -35,5 +37,19 @@ export class Tab2Page {
       }]
     });
     await actionSheet.present();
+  }
+  login () {
+    console.log(this.faio)
+    this.faio.show({
+      title: 'Biometric Authentication', // (Android Only) | optional | Default: "<APP_NAME> Biometric Sign On"
+      subtitle: 'Coolest Plugin ever', // (Android Only) | optional | Default: null
+      description: 'Please authenticate' ,// optional | Default: null
+      fallbackButtonTitle: 'Use Backup', // optional | When disableBackup is false defaults to "Use Pin".
+                                         // When disableBackup is true defaults to "Cancel"
+      disableBackup:true,  // optional | default: false
+  })
+  .then((result: any) => console.log(result))
+  .catch((error: any) => console.log(error));
+  
   }
 }
